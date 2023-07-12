4ad93d3bc442c890d2452db3ae609ad70edb66b7
liaomazel <mazel.li@myirtech.com>
2023-07-12 09:30:43 +0800

    FEAT: add Quectel 4G&5G WWAN

--- drivers/usb/serial/option.c
+++ drivers/usb/serial/option.c
@@ -1098,6 +1098,8 @@ static const struct usb_device_id option_ids[] = {
 	  .driver_info = RSVD(4) },
 	{ USB_DEVICE(QUECTEL_VENDOR_ID, QUECTEL_PRODUCT_EC25),
 	  .driver_info = RSVD(4) },
+	{ USB_DEVICE(QUECTEL_VENDOR_ID, 0x0800),
+		.driver_info = RSVD(4) },
 	{ USB_DEVICE(QUECTEL_VENDOR_ID, QUECTEL_PRODUCT_EG95),
 	  .driver_info = RSVD(4) },
 	{ USB_DEVICE(QUECTEL_VENDOR_ID, QUECTEL_PRODUCT_BG96),
